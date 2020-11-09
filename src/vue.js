e2eb0c915144ab7c9cb80db7f20ee9dc7b3d3560
liushanshan <shanshan.liu@datacube.hk>
2020-11-09 21:30:33 +0800

    新增webpack 对css 和图片的打包

--- src/vue.js
+++ src/vue.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
+import test from './img/test.jpg'
+import './style/index.scss'
 new Vue({
-     render: h=><h2>hello jsx</h2>
+     render: h=><div>
+        <h2>hello jsx</h2>  
+        <img src={test}></img>
+          </div>
 }).$mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
-const path=require('path')
+const path = require('path')
 module.exports = {
     // entry:'./src/index.js',
     // output:{
     //     path:path.join(__dirname,'dist'),
     //     filename:'bundle.js'
     // }, //单入口
-    entry:{
-        index:'./src/index.js',
-        search:'./src/search.js',
-        vue:'./src/vue.js'
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js',
+        vue: './src/vue.js'
     },
-    output:{
-        path:path.join(__dirname,'dist'),
-        filename:'[name].js'
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     }, // 多入口
-    mode:'production',
-    module:{
-        rules:[
+    mode: 'production',
+    module: {
+        rules: [{
+                test: /\.js$/, // 匹配js文件
+                use: 'babel-loader' // 使用babel-loader 进行解析
+            }, {
+                test: /.css$/,
+                use: [
+                    
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
             {
-                test:/\.js$/, // 匹配js文件
-                use:'babel-loader' // 使用babel-loader 进行解析
+                test: /.(png|jpg|gif)$/,
+                use: 'file-loader'
             }
         ]
     }

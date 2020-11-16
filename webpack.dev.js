3b38233c13046ee3b2e69204ea26aa3ceac8f542
liushanshan <shanshan.liu@datacube.hk>
2020-11-16 21:41:03 +0800

    多页面打包待完成

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     // entry:'./src/index.js',
     // output:{
@@ -7,9 +8,9 @@ module.exports = {
     //     filename:'bundle.js'
     // }, //单入口
     entry: {
-        index: './src/index.js',
-        search: './src/search.js',
-        vue: './src/vue.js'
+         index: './src/index/index.js',
+             search: './src/search/index.js',
+             vue: './src/vue/index.js'
     },
     output: {
         path: path.join(__dirname, 'dist'),
@@ -43,6 +44,27 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(), // 热更新
+         new htmlWebpackPlugin({
+                 filename: 'index.html',
+                 template: path.join(__dirname, './src/index/index.html'),
+                 chunks: ['index'],
+                 inject: true,
+                 minify: true
+             }),
+             new htmlWebpackPlugin({
+                 filename: 'vue.html', // 打包输出文件名
+                 template: path.join(__dirname, './src/vue/index.html'), // 模板
+                 chunks: ['vue'], // 需要注入的chunk (js/css)
+                 inject: true, // 是否需要将chunk自动注入到html中
+                 minify: true
+             }),
+             new htmlWebpackPlugin({
+                 filename: 'search.html',
+                 template: path.join(__dirname, './src/search/index.html'),
+                 chunks: ['search'],
+                 inject: true,
+                 minify: true
+             }),
     ],
     devServer: {
         contentBase: './dist',
